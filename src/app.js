ab7b4e7d5b1c8e7bd131b6c07e9a9d7ef0cf5726
Dev leo <arasudevraj75@gmail.com>
2019-04-22 01:47:27 +0530

    changes to start path

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geoCode = require('./utils/geoCode');
 const getForecast = require('./utils/forecast');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 //Define path
 const publicDir = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views')
