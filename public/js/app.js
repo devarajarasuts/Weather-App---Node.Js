21e22c5d9173083494af38d94d9a0d98b5eb63f2
Dev leo <arasudevraj75@gmail.com>
2019-04-22 00:30:56 +0530

    Second Commit static app.js changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
-const weatherForm = document.querySelector('form')
-const search = document.querySelector('input')
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
 
 const messageOne = document.querySelector('#message-one');
 const messageTwo = document.querySelector('#message-two');
